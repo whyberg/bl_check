ddbe636bd4191c9eedb83d00dc4b5901a70121da
Andrey Artemyev <whyberg@gmail.com>
2021-07-08 12:08:06 +0600

    Add multi net check support

--- bl_check.pl
+++ bl_check.pl
@@ -5,8 +5,7 @@ use warnings;
 use Socket;
 use LWP::UserAgent;
 
-my %rkn;
-
+my %NETS;
 # Load config
 open CFG, "./bl_check.conf" or die "Create bl_check.conf";
 my $config  = join "",<CFG>;
@@ -14,26 +13,39 @@ close CFG;
 eval $config;
 die "Couldn't interpret the configuration file.\nError details follow: $@\n" if $@;
 
+my %rkn;
 # load RKN list
 my $ua = LWP::UserAgent->new(ssl_opts => { verify_hostname => 1 });
 $ua->agent('Wget/1.12 (linux-gnu)');
 my $res = $ua->get("https://reestr.rublacklist.net/api/v2/ips/csv");
- die "Couldn't get it!" unless defined $res;
-
-
+ die "Couldn't get rkn blacklist!" unless defined $res;
 foreach (split(",",$res->decoded_content)) {
     $_=~s/[\n\r]//g;
     $rkn{$_}=1;
 }
 
-my $net_start = unpack("N",inet_aton("104.24.118.0"));
-my $net_stop  = unpack("N",inet_aton("104.24.118.255"));
-
-for (my $i=$net_start; $i<=$net_stop; $i+=1) {
-    my $addr = inet_ntoa(pack("N",$i));
-    my $res = check($addr);
-    if ( $res ne "" ) {
-        printf("ip %s in lists %s\n",$addr,$res);
+foreach (sort(keys(%NETS))) {
+    my $full_mask  = unpack( "N", pack( "C4", 255,255,255,255 ) );
+    my $net_start;
+    my $net_stop;
+    my ($ip_t,$mask_t) = split("/",$NETS{$_});
+    if ( defined($mask_t) ) {
+        my $mask = 1;
+        $mask = ( 2 ** (32 - $mask_t) ) - 1;
+        $net_start = unpack("N",inet_aton($ip_t)) & ( $full_mask ^ $mask );
+        $net_stop = unpack("N",inet_aton($ip_t)) | $mask;
+        printf "Check range: from %s to %s\n",inet_ntoa(pack("N",$net_start)),inet_ntoa(pack("N",$net_stop));
+    } else {
+        $net_start = unpack("N",inet_aton($ip_t));
+        $net_stop = $net_start;
+        printf "Check ip: %s\n",inet_ntoa(pack("N",$net_start));
+    }
+    for (my $i=$net_start; $i<=$net_stop; $i+=1) {
+        my $addr = inet_ntoa(pack("N",$i));
+        my $res = check($addr);
+        if ( $res ne "" ) {
+            printf("ip %s in lists %s\n",$addr,$res);
+        }
     }
 }
 
@@ -86,7 +98,8 @@ sub check_sorbs {
 }
 
 sub check_zapbl {
-    my $addr = sprintf("%s..dnsbl.zapbl.net",inet_reverse(shift));
+    my $addr = sprintf("%s.dnsbl.zapbl.net",inet_reverse(shift));
+    print $addr;
     my $packed_ip = gethostbyname($addr);
     if (defined $packed_ip) {
         return "ZAPBS_DNSBL ";
@@ -102,12 +115,3 @@ sub check_spfbl {
         }
     }
 }
-
-#    SpamCop: www.spamcop.net
-#    SBL (SpamHaus Blocklist): https://www.spamhaus.org/sbl/
-#    PBL (Spamhaus Policy Block List): https://www.spamhaus.org/pbl/
-#    SORBS (Spam and Open Relay Blocking System: www.sorbs.net
-#    OPM (Open Proxy Monitor List): www.blitzed.org
-#    CBL (Composite Blocking List): cbl.abuseat.org
-#    Five-Ten Blocklist: www.five-ten-sg.com
-#    MAPS (Trend Micro DNSBL): https://www.ers.trendmicro.com/
